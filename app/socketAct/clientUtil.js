3e156aeac44c9c0821af317b50289e9c81656e4f
Dang Le <dangapolo@gmail.com>
2014-07-30 08:18:09 -0700

    add util classes for socket

---
+++ app/socketAct/clientUtil.js
@@ -0,0 +1,41 @@
+module.exports = function (io, client, db) {
+  var GameProcess = db.GameProcess;
+  var clientHelper = require('../app/socketAct/clientHelper')(io, client, db);
+
+  client.on('disconnect', onDisconnect);
+  client.on('day chat', onDayChat);
+  client.on('night chat', onNightChat);
+
+
+
+
+  function onDayChat(data) {
+    client.broadcast.to(data.gameID).emit('day chat', {userName: data.userName, msg: data.msg});
+  }
+
+  function onNightChat(data) {
+    var playerList = GameProcess[data.gameID].votePlayers;
+
+    for (var player in playerList) {
+      if (GameProcess[data.gameID].badSide.hasOwnProperty(player) && player !== data.userName) {
+        io.sockets.socket(GameProcess[data.gameID].badSide[player]).emit('night chat', {userName: data.userName, msg: data.msg})
+      }
+    }
+  }
+
+  function onDisconnect() {
+    User.find({socket: client.id }, function (err, userInfo) {
+      if (!err && userInfo.length > 0) {
+        var user = userInfo[0];
+        if (user.inState && user.inState !== 'LOBBY') {
+          clientUtil.onLeaveGame(client, {gameID: user.inGame, userName: user.userName});
+        };
+      };
+
+    });
+  }
+
+
+
+
+}
